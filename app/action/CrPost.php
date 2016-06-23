dd700384786e09438f73f4e0685062a8e8b97756
Klaas Van Parys <warsaalk@gmail.com>
2016-06-23 23:34:41 +0200

    Removed old comment

--- app/action/CrPost.php
+++ app/action/CrPost.php
@@ -7,34 +7,7 @@ use Kokx\Service\CombatReport;
 use Kokx\Renderer\Helper;
 
 class CrPost extends ActionType {
-/*
-$vl = $this->Main()->getValidator();
 
-$vl->addVariable( 'report', array('min=1'), 'string' );
-$vl->addVariable( 'theme', array('select'), 'string' );
-$vl->addVariable( 'middletext', array(), 'string', false );
-$vl->addVariable( 'merge', array(), 'integer', false );
-$vl->addVariable( 'hidetime', array(), 'integer', false ); // Use integer type for this checkbox as it doesn't exits in a group
-$vl->addVariable( 'advanced', array(), 'integer', false );
-$vl->addVariable( 'spoiler', array(), 'integer', false );
-$vl->addVariable( 'raids', array(), 'string', false );
-$vl->addVariable( 'attacker_harvest', array(), 'string', false );
-$vl->addVariable( 'attacker_deuterium', array(), 'string', false );
-$vl->addVariable( 'defender_harvest', array(), 'string', false );
-$vl->addVariable( 'defender_deuterium', array(), 'string', false );
-
-$vl->validate( $form );
-
-if( $vl->isValid() ){
-
-	//$this->Main()->addInfo( new Info( 'Valid Cr', Info::SUCCESS ) );
-
-}else{
-
-	$this->Main()->addInfo( new Info( $this->Main()->getDict()->getVal('Invalid post'), Info::ERROR ) );
-
-}
-	*/
 	/**
 	 * (non-PHPdoc)
 	 * @see \Plinth\Request\ActionType::getSettings()
